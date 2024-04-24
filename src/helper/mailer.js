343cf42786d01bbe37b9c778827059e481ea9f7a
Gm kandhro <gmkandhro182@gmail.com>
2024-04-24 08:43:20 +0500

    project completed after adding mongodb cloud

--- src/helper/mailer.js
+++ src/helper/mailer.js
@@ -32,10 +32,10 @@ export const SendMail = async({email,emailType,userId})=>{
       });
 
           const mailerInfo = {
-            from: "gmkandhro182@gmail.com",  
+            from: "gmkandhro88@gmail.com",  
             to: email,
             subject: emailType === "VERIFY" ?  `Email Verification Code For Your Account`: `Password Reset Link`,
-            html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
+            html: `<p>Click <a href="${process.env.DOMAIN}/verify?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
             or copy and paste the link below in your browser. <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
             </p>`
 
